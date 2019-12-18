ee9da2728f232b561945256e51d97c35b25f5c0e
Daleb015 <daleb015@gmail.com>
2019-12-17 22:13:15 -0500

    Added lombok

---
+++ src/main/java/com/daleb/ereservation/model/Cliente.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.daleb.ereservation.model;
+
+import lombok.Data;
+
+/**
+ * Clase que representa la tabla cliente
+ * @author daleb
+ *
+ */
+@Data
+public class Cliente {
+  String nombreCli;
+  String apellidoCli;
+}
