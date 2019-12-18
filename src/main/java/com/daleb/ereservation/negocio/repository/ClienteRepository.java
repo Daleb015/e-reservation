1f217888c3716afddfd68fc34838d55472cdd5a1
Daleb015 <daleb015@gmail.com>
2019-12-18 00:05:24 -0500

    added querys jpa

--- src/main/java/com/daleb/ereservation/negocio/repository/ClienteRepository.java
+++ src/main/java/com/daleb/ereservation/negocio/repository/ClienteRepository.java
@@ -3,6 +3,8 @@
  */
 package com.daleb.ereservation.negocio.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.daleb.ereservation.model.Cliente;
@@ -13,5 +15,15 @@ import com.daleb.ereservation.model.Cliente;
  *
  */
 public interface ClienteRepository extends JpaRepository<Cliente,String> {
+  
+  /**
+   * Definicion de metodo para buscar clientes por apellido
+   * @param apellidoCli
+   * @return
+   */
+  public List<Cliente> findByApellidoCli(String apellidoCli);
+  
+  public Cliente findByIdentificacion(String identificacionCli);
+  
 
 }
--- src/main/java/com/daleb/ereservation/negocio/repository/ReservaRepository.java
+++ src/main/java/com/daleb/ereservation/negocio/repository/ReservaRepository.java
@@ -3,7 +3,12 @@
  */
 package com.daleb.ereservation.negocio.repository;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.daleb.ereservation.model.Reserva;
 
@@ -14,5 +19,14 @@ import com.daleb.ereservation.model.Reserva;
  *
  */
 public interface ReservaRepository extends JpaRepository<Reserva, String> {
+
+  /**
+   * 
+   * @param fechaInicio
+   * @param fechaSalida
+   * @return Listado de reservas que coinciden con los parametros de fechas
+   */
+  @Query("Select r from Reserva r where r.fechaIngresoRes =:fechaInicio and r.fechaSalidaRes =: fechaSalida")
+  public List<Reserva> find(@Param("fechaInicio") Date fechaInicio,@Param("fechaSalida") Date fechaSalida);
 
 }
