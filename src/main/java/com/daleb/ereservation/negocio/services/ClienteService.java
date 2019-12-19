efa3f0ee4fc03495e6bca32ba7c232be562d2b9b
Daleb015 <daleb015@gmail.com>
2019-12-18 22:16:04 -0500

    Added swagger configuration

--- src/main/java/com/daleb/ereservation/negocio/services/ClienteService.java
+++ src/main/java/com/daleb/ereservation/negocio/services/ClienteService.java
@@ -5,6 +5,7 @@ package com.daleb.ereservation.negocio.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.daleb.ereservation.model.Cliente;
 import com.daleb.ereservation.negocio.repository.ClienteRepository;
@@ -17,6 +18,7 @@ import com.daleb.ereservation.negocio.repository.ClienteRepository;
  */
 
 @Service
+@Transactional(readOnly = true)
 public class ClienteService {
 
   @Autowired
@@ -24,41 +26,44 @@ public class ClienteService {
 
   /**
    * Método para guardar un cliente
+   * 
    * @param cliente
    * @return
    */
+  @Transactional
   public Cliente create(Cliente cliente) {
     return clienteRepository.save(cliente);
   }
-  
+
   /**
    * Para actualizar un cliente, en este caso el cliente entrante tiene id
+   * 
    * @param cliente
    * @return
    */
+  @Transactional
   public Cliente update(Cliente cliente) {
     return clienteRepository.save(cliente);
   }
-  
-  
+
   /**
    * Método para eliminar un cliente
+   * 
    * @param cliente
    */
+  @Transactional
   public void delete(Cliente cliente) {
     clienteRepository.delete(cliente);
   }
-  
-  
+
   /**
    * Método para ubicar un cliente por identificacion
+   * 
    * @param identificationCli
    * @return
    */
   public Cliente findByIdentification(String identificationCli) {
     return clienteRepository.findByIdentificacion(identificationCli);
   }
-  
-  
 
 }
